d52b7df6ede3c53f6d77743ee3bffd32326d096f
chuchu02 <rlaxogns73@gmail.com>
2022-10-03 18:25:41 +0900

    ScrollView에 대한 여러가지 Style 넣기

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Dimensions,
 } from 'react-native';
 
 import {
@@ -26,13 +27,19 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <ScrollView horizontal style={styles.weather}>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
@@ -69,18 +76,18 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: '500',
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: 'center',
   },
   temp: {
+    color: 'black',
     marginTop: 50,
     fontSize: 178,
   },
   description: {
+    color: 'black',
     marginTop: -30,
     fontSize: 60,
   },
